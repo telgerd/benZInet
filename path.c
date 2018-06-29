5e96d7d3541bf41c3f7e79048a1ba6d36ceb1632
telgerd <saied.h.telgerd@gmail.com>
2018-06-29 16:46:35 +0200

    It's first commit

---
+++ path.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+#include "myfunctions.h"
+
+int main(){
+    //printfunction();
+    //Variables();
+    //array();
+    //printf("My name is %s\n", myname); //Define a global variable
+    //scanfunction();
+    //Shorthandifelse();
+    //Increment_Operator();
+    //continuefunction();
+    //Rounding_num();
+    //absolutefunction();
+    //mathfunctions();
+    //randomfunction();
+    //intflo_array();
+    //dynamic_array();
+    //sort_function();
+    //Pointers();
+    //array_pointers();
+    //string_pointers();
+    fgets_strings();
+    return 0;
+}
